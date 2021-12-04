c1dfc83fed477a99a25e7d761d9a7ac6dfe4a2e8
KYUNGMIN NA <nkm4095@naver.com>
2021-12-04 15:30:56 +0900

    Home Screen Function List

--- app/src/main/java/com/android/mungmung_dictionary/Function_List.java
+++ app/src/main/java/com/android/mungmung_dictionary/Function_List.java
@@ -3,13 +3,6 @@ package com.android.mungmung_dictionary;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.android.mungmung_dictionary.ui.Food.FoodFragment;
-import com.android.mungmung_dictionary.ui.My.MyFragment;
-import com.android.mungmung_dictionary.ui.feed.FeedFragment;
-import com.android.mungmung_dictionary.ui.home.HomeFragment;
-import com.android.mungmung_dictionary.ui.yesno.YesnoFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,27 +10,32 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.android.mungmung_dictionary.databinding.ActivityBottomBinding;
+import com.android.mungmung_dictionary.ui.Description.Dog_Food;
+import com.android.mungmung_dictionary.ui.Dog_Food_Judgment.Dog_Food_harmfulness_Judgment;
+import com.android.mungmung_dictionary.ui.Snack.Dog_Snack;
+import com.android.mungmung_dictionary.ui.My_Dog_Information.My_Dog_Information;
+import com.android.mungmung_dictionary.ui.Brand_List.Brand_List;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class BottomActivity extends AppCompatActivity {
+public class Function_List extends AppCompatActivity {
 
     private FragmentManager fragmentManager = getSupportFragmentManager();
 
     //5가지 fragment 객체 선언
-    private HomeFragment homeFragment = new HomeFragment();
-    private FoodFragment foodFragment = new FoodFragment();
-    private MyFragment myFragment = new MyFragment();
-    private FeedFragment feedFragment = new FeedFragment();
-    private YesnoFragment yesnoFragment = new YesnoFragment();
+    private Brand_List dogFoodBrandList = new Brand_List();
+    private Dog_Snack dogSnack = new Dog_Snack();
+    private My_Dog_Information myDogInformation = new My_Dog_Information();
+    private Dog_Food dogFoodMain = new Dog_Food();
+    private Dog_Food_harmfulness_Judgment dogFoodharmfulnessJudgment = new Dog_Food_harmfulness_Judgment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom);
+        setContentView(R.layout.activity_function_list);
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frame_layout, homeFragment).commitAllowingStateLoss();
+        transaction.replace(R.id.frame_layout, dogFoodBrandList).commitAllowingStateLoss();
 
         // 메뉴 아이템이 선택될때 호출될 리스너 등록
         // home, heart, person, star, drink 다섯가지 fragment
@@ -47,24 +45,24 @@ public class BottomActivity extends AppCompatActivity {
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
                 switch (menuItem.getItemId()) {
                     case R.id.navigation_home: {
-                        transaction.replace(R.id.frame_layout, homeFragment).commitAllowingStateLoss();
+                        transaction.replace(R.id.frame_layout, dogFoodBrandList).commitAllowingStateLoss();
                         break;
                     }
                     case R.id.navigation_food: {
-                        transaction.replace(R.id.frame_layout, foodFragment).commitAllowingStateLoss();
+                        transaction.replace(R.id.frame_layout, dogSnack).commitAllowingStateLoss();
                         break;
                     }
                     case R.id.navigation_feed: {
 
-                        transaction.replace(R.id.frame_layout, feedFragment).commitAllowingStateLoss();
+                        transaction.replace(R.id.frame_layout, dogFoodMain).commitAllowingStateLoss();
                         break;
                     }
                     case R.id.navigation_my: {
-                        transaction.replace(R.id.frame_layout, myFragment).commitAllowingStateLoss();
+                        transaction.replace(R.id.frame_layout, myDogInformation).commitAllowingStateLoss();
                         break;
                     }
                     case R.id.navigation_yesno: {
-                        transaction.replace(R.id.frame_layout, yesnoFragment).commitAllowingStateLoss();
+                        transaction.replace(R.id.frame_layout, dogFoodharmfulnessJudgment).commitAllowingStateLoss();
                         break;
                     }
                 }
